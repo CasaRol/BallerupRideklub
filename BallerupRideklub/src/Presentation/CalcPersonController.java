da583ca1ad8054891aadb918704ade2704da7caf
CasaRol <alexander-rol@hotmail.com>
2019-01-27 15:39:02 +0100

    lots of changes

--- BallerupRideklub/src/Presentation/CalcPersonController.java
+++ BallerupRideklub/src/Presentation/CalcPersonController.java
@@ -8,11 +8,14 @@ package Presentation;
 import Acquaintence.IController;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
@@ -28,33 +31,11 @@ import javafx.stage.Stage;
 public class CalcPersonController implements IController, Initializable {
 
     @FXML
-    private ToggleGroup Consent;
-    @FXML
-    private Label label_Information;
-    @FXML
-    private RadioButton rbtn_concentYes;
-    @FXML
-    private RadioButton rbtn_concentNo;
-    @FXML
     private Button btn_back;
     @FXML
     private TextField txt_firstName;
     @FXML
     private TextField txt_lastName;
-    @FXML
-    private TextField txt_weight;
-    @FXML
-    private TextField txt_height;
-    @FXML
-    private TextField txt_BMI;
-    @FXML
-    private RadioButton rbtn_beginner;
-    @FXML
-    private ToggleGroup level;
-    @FXML
-    private RadioButton rbtn_trained;
-    @FXML
-    private RadioButton rbtn_expert;
     @FXML
     private RadioButton rbtn_balanceYes;
     @FXML
@@ -65,23 +46,72 @@ public class CalcPersonController implements IController, Initializable {
     private Button btn_calculate;
     @FXML
     private Button btn_cancel;
+    @FXML
+    private ComboBox<?> comb_weight;
+    @FXML
+    private ComboBox<?> comb_height;
+    @FXML
+    private ComboBox<?> comb_level;
+    @FXML
+    private Label label_warning;
 
     /**
      * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        label_Information.setText("\uD83D");
-    } 
-    
+        
+        //Fill weightBox and set visible limit
+        comb_weight.getItems().addAll(fillWeightBox());
+        comb_weight.setVisibleRowCount(5);
+        comb_weight.getSelectionModel().selectFirst();
+
+        //Fill heightBox and set visible limit
+        comb_height.getItems().addAll(fillHeightBox());
+        comb_height.setVisibleRowCount(5);
+        comb_height.getSelectionModel().selectFirst();
+
+        //Fill levelBox
+        comb_level.getItems().addAll(fillLevelBox());
+        comb_level.getSelectionModel().selectFirst();
+    }
+
     @Override
     public void injectStage(Stage stage) {
         PresentationFacade.stage = stage;
     }
 
-    @FXML
-    private void OpenInformation(MouseEvent event) {
-        
+    private List fillWeightBox() {
+        ArrayList weightList = new ArrayList();
+        weightList.add("Vælg kg");
+        for (int i = 40; i <= 130; i++) {
+            weightList.add(i);
+        }
+
+        return weightList;
+    }
+
+    private List fillHeightBox() {
+        ArrayList heightList = new ArrayList();
+        heightList.add("Vælg cm");
+        for (int i = 50; i <= 230; i++) {
+            heightList.add(i);
+        }
+
+        return heightList;
+    }
+
+    private List fillLevelBox() {
+        ArrayList levelList = new ArrayList();
+        levelList.add("Vælg niveau");
+        levelList.add("Begynder");
+        levelList.add("Letøvet");
+        levelList.add("Øvet");
+
+        return levelList;
     }
 
     @FXML
@@ -91,24 +121,25 @@ public class CalcPersonController implements IController, Initializable {
 
     @FXML
     private void calcScore(ActionEvent event) {
+//        System.out.println(comb_weight.getSelectionModel().getSelectedItem().toString());
+        System.out.println("calcScore entered");
+        if (!(comb_weight.getSelectionModel().getSelectedItem().toString().equals("Vælg kg")) && !(comb_height.getSelectionModel().getSelectedItem().toString().equals("Vælg cm")) && !(comb_level.getSelectionModel().getSelectedItem().toString().equals("Vælg niveau"))) {
+            System.out.println("inside if");
+        } else {
+            System.out.println("outside if");
+            label_warning.setText("Venligst udfyld alle informationerne");
+        }
     }
 
     @FXML
     private void clearInfo(ActionEvent event) {
         txt_firstName.clear();
         txt_lastName.clear();
-        txt_weight.clear();
-        txt_height.clear();
-        rbtn_beginner.setSelected(false);
-        rbtn_trained.setSelected(false);
-        rbtn_expert.setSelected(false);
-        txt_BMI.clear();
         rbtn_balanceYes.setSelected(false);
         rbtn_balanceNo.setSelected(false);
-        rbtn_concentYes.setSelected(false);
-        rbtn_concentNo.setSelected(false);
+        comb_weight.getSelectionModel().selectFirst();
+        comb_height.getSelectionModel().selectFirst();
+        comb_level.getSelectionModel().selectFirst();
     }
 
-    
-    
 }
